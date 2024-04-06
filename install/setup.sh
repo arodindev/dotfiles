ca32bfead9f77d0c7b231488cbf90bcd724efa2d
arodindev <arodin.devv@gmail.com>
2024-04-06 08:38:53 +0200

    add pyenv

--- install/setup.sh
+++ install/setup.sh
@@ -7,7 +7,7 @@ echo "== INSTALL OH-MY-ZSH =="
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
 command -v zsh | sudo tee -a /etc/shells
-chsh -s $(which zsh) # make zsh default
+chsh -s "$(which zsh)" # make zsh default
 
 echo "== INSTALL OTHER OH-MY-ZSH PLUGINS =="
 git clone https://github.com/zsh-users/zsh-autosuggestions "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}"/plugins/zsh-autosuggestions
